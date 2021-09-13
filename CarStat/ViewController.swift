d4bcf7771a15ed727ae11875c1b1d5dbf86a3f6a
Aleksey Mironov <77401319+mirlex1986@users.noreply.github.com>
2021-09-13 18:04:02 +0300

    Initial Commit

---
+++ CarStat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarStat
+//
+//  Created by Aleksey Mironov on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
